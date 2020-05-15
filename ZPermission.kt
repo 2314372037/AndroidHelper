e8c75e6c140995e8b557167ffa12f6a727b644e3
zhanghao <zh2314372037@outlook.com>
2020-05-15 21:05:21 +0800

    init...

---
+++ ZPermission.kt
@@ -0,0 +1,126 @@
+package com.zh.mvvmcore
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
+
+/****
+ * 基于viewModel和liveData的权限请求
+ * 自动管理生命周期
+ *  created by zhanghao
+ */
+class ZPermission private constructor(private val activity: AppCompatActivity) : Fragment() {
+    private lateinit var vm: VM
+    private val requestCode = 9990
+    private var permissions: Array<out String>? = null
+    private val TAG = this::class.simpleName
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = View(context)
+        view.setBackgroundColor(Color.TRANSPARENT)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vm = ViewModelProviders.of(this.activity).get(VM::class.java)
+        vm.allow.observe(this.activity, Observer {
+            if (it) {
+                allowListener.invoke()
+            } else {
+                refuseListener.invoke(vm.refusePermissions)
+            }
+        })
+
+        if (!permissions.isNullOrEmpty()) {
+            requestPermissions(permissions!!, requestCode)
+        }
+    }
+
+    override fun onDestroyView() {
+        Log.w(TAG, "onDestroyView")
+        super.onDestroyView()
+    }
+
+    companion object {
+        private var zPermission: ZPermission? = null
+        private lateinit var allowListener: () -> Unit
+        private lateinit var refuseListener: (list: ArrayList<String>?) -> Unit
+
+        fun get(activity: Activity): ZPermission? {
+            if (zPermission == null) {
+                if (activity is AppCompatActivity) {
+                    zPermission = ZPermission(activity)
+                    activity.supportFragmentManager.beginTransaction()
+                        .add(zPermission!!, "permissionF").commit()
+                } else {
+                    Log.w("ZPermission", "警告：传入的Activity不是一个AppCompatActivity类，取消本次请求")
+                    return null
+                }
+            }
+            return zPermission
+        }
+    }
+
+
+    /***
+     * 请求权限
+     */
+    fun req(vararg permissions: String): ZPermission {
+        this.permissions = permissions
+        return this
+    }
+
+    fun listener(allow: () -> Unit, refuse: ((list: ArrayList<String>?) -> Unit)? = null) {
+        allowListener = allow
+
+        if (refuse!=null){
+            refuseListener = refuse
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestCode) {
+            for (i in permissions) {
+                //如果授予权限
+                if (ContextCompat.checkSelfPermission(
+                        activity,
+                        i
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    vm.allowPermissions.add(i)
+                } else {
+                    vm.refusePermissions.add(i)
+                }
+            }
+            vm.allow.value = permissions.size == vm.allowPermissions.size
+
+            activity.supportFragmentManager.beginTransaction().remove(this).commit()
+        }
+    }
+
+    private class VM : ViewModel() {
+        var refusePermissions: ArrayList<String> = arrayListOf()
+        var allowPermissions: ArrayList<String> = arrayListOf()
+        val allow = MutableLiveData<Boolean>()
+    }
+
+}
